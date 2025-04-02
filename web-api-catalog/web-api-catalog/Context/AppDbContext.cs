98abde137009d4d1f815366338818e4724d882f5
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-04-02 18:45:54 -0300

    Add models and DbContext for Category and Product
    Updated `Category.cs` and `Product.cs` to include proper namespace declarations and class definitions with relevant properties. Modified `web-api-catalog.csproj.user` to add scaffolding settings. Introduced `AppDbContext.cs` to manage database operations for `Category` and `Product` entities.

---
+++ web-api-catalog/web-api-catalog/Context/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using web_api_catalog.Models;
+
+namespace web_api_catalog.Context;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+    {
+    }
+
+    public DbSet<Category>? categories { get; set; }
+    public DbSet<Product>? products { get; set; }
+
+}
