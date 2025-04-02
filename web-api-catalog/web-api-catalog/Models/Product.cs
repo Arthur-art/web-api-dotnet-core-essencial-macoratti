a380b42effa74227ba1bffcf2f21f27726365b31
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-04-02 13:53:55 -0300

    Remove weather functionality and add product models
    This commit removes the weather forecasting functionality by deleting the `WeatherForecastController` and `WeatherForecast` class. A new folder for controllers has been added to the project structure. Additionally, new `Category` and `Product` classes have been introduced to support product management, enhancing the application's capabilities.

---
+++ web-api-catalog/web-api-catalog/Models/Product.cs
@@ -0,0 +1,18 @@
+﻿namespace web_api_catalog.Models;
+
+public class Product
+{
+    public int ProductId { get; set; }
+
+    public string? Name { get; set; }
+
+    public string? Description { get; set; }
+
+    public decimal Price { get; set; }
+
+    public string? ImageUrl { get; set; }
+
+    public float Rating { get; set; }
+
+    public DateTime DateOfRegistration { get; set; }
+}
