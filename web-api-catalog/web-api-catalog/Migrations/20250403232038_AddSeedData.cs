354ac862d4662bf4aacfbfdd4d5f56974e2ba9e6
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-04-03 20:29:16 -0300

    Add migration for seeding category data
    Introduces the `AddSeedData` migration class to insert three categories ('Bebidas', 'Lanches', 'Sobremesas') into the `categories` table. The `Down` method allows for reverting these changes by deleting all entries. Updates to the `AddSeedData.Designer.cs` file include necessary metadata for tracking the migration.

---
+++ web-api-catalog/web-api-catalog/Migrations/20250403232038_AddSeedData.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web_api_catalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedData : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into categories(Nome, ImagemUrl) Values('Bebidas', 'bebidas.png')");
+            mb.Sql("insert into categories(Nome, ImagemUrl) Values('Lanches', 'lanches.png')");
+            mb.Sql("insert into categories(Nome, ImagemUrl) Values('Sobremesas', 'sobremesas.png')");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from categories");
+        }
+    }
+}
