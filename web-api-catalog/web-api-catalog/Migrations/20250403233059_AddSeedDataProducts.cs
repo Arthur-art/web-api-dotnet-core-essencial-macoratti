2a41761405efae69f43f0f0d7009f9cf567d4fbc
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-04-03 20:36:12 -0300

    Add migration to seed initial product data
    This commit introduces the `AddSeedDataProducts` migration class, which seeds the database with three products: "Coca-Cola", "X-Burguer", and "Pudim". Each product entry includes attributes such as name, description, price, image URL, rating, date of registration, and category ID. A `Down` method is also provided to delete all products when rolling back the migration. Additionally, an auto-generated designer file is included to define the migration and ensure the correct model structure for the `Product` and `Category` entities.

---
+++ web-api-catalog/web-api-catalog/Migrations/20250403233059_AddSeedDataProducts.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web_api_catalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSeedDataProducts : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mb)
+        {
+            mb.Sql("insert into products(Name, Description, Price, ImageUrl, Rating, DateOfRegistration, CategoryId) Values('Coca-Cola', 'Refrigerante de cola', 5.00, 'coca-cola.png', 10, now(), 1)");
+            mb.Sql("insert into products(Name, Description, Price, ImageUrl, Rating, DateOfRegistration, CategoryId) Values('X-Burguer', 'Sanduíche de carne com queijo', 15.00, 'x-burguer.png', 20, now(), 2)");
+            mb.Sql("insert into products(Name, Description, Price, ImageUrl, Rating, DateOfRegistration, CategoryId) Values('Pudim', 'Sobremesa de leite condensado', 8.00, 'pudim.png', 30, now(), 3)");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mb)
+        {
+            mb.Sql("delete from products");
+        }
+    }
+}
