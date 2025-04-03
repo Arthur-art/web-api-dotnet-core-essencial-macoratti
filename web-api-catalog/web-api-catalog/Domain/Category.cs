6321bb436fe57593e89afe1a924d27324c2590fb
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-04-03 20:09:09 -0300

    Add data annotations and update database schema
    This commit introduces data annotations to the `Category` and `Product` classes, enforcing validation rules and specifying database column properties. The migration files rename tables to PascalCase and update column types to `varchar` with specified lengths, while adjusting the `Price` column to `decimal(10,2)`. The `AppDbContextModelSnapshot.cs` file is also updated to reflect these changes, enhancing data integrity and ensuring the database schema aligns with application requirements.

--- web-api-catalog/web-api-catalog/Domain/Category.cs
+++ web-api-catalog/web-api-catalog/Domain/Category.cs
@@ -1,17 +1,25 @@
 ﻿using System.Collections.ObjectModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace web_api_catalog.Models;
 
+[Table("Categories")]
 public class Category
 {
     public Category()
     {
         Products = new Collection<Product>();
     }
-    public int CategoryId { get; set; }
 
+    [Key]
+    public int CategoryId { get; set; }
+    [Required]
+    [StringLength(80)]
     public string? Nome { get; set; }
 
+    [Required]
+    [StringLength(300)]
     public string? ImagemUrl { get; set; }
 
     public ICollection<Product>? Products { get; set; }
---
+++ web-api-catalog/web-api-catalog/Migrations/20250403230815_ImplementDataAnnotations.cs
@@ -0,0 +1,270 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace web_api_catalog.Migrations
+{
+    /// <inheritdoc />
+    public partial class ImplementDataAnnotations : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_products",
+                table: "products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_categories",
+                table: "categories");
+
+            migrationBuilder.RenameTable(
+                name: "products",
+                newName: "Products");
+
+            migrationBuilder.RenameTable(
+                name: "categories",
+                newName: "Categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_products_CategoryId",
+                table: "Products",
+                newName: "IX_Products_CategoryId");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "Products",
+                type: "decimal(10,2)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(65,30)");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Name",
+                keyValue: null,
+                column: "Name",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "Products",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "ImageUrl",
+                keyValue: null,
+                column: "ImageUrl",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "Products",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Products",
+                keyColumn: "Description",
+                keyValue: null,
+                column: "Description",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "Products",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "Nome",
+                keyValue: null,
+                column: "Nome",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "Categories",
+                type: "varchar(80)",
+                maxLength: 80,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.UpdateData(
+                table: "Categories",
+                keyColumn: "ImagemUrl",
+                keyValue: null,
+                column: "ImagemUrl",
+                value: "");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "Categories",
+                type: "varchar(300)",
+                maxLength: 300,
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "longtext",
+                oldNullable: true)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Products",
+                table: "Products",
+                column: "ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products",
+                column: "CategoryId",
+                principalTable: "Categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Products_Categories_CategoryId",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Products",
+                table: "Products");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Categories",
+                table: "Categories");
+
+            migrationBuilder.RenameTable(
+                name: "Products",
+                newName: "products");
+
+            migrationBuilder.RenameTable(
+                name: "Categories",
+                newName: "categories");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Products_CategoryId",
+                table: "products",
+                newName: "IX_products_CategoryId");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "Price",
+                table: "products",
+                type: "decimal(65,30)",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "decimal(10,2)");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "products",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(80)",
+                oldMaxLength: 80)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImageUrl",
+                table: "products",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Description",
+                table: "products",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Nome",
+                table: "categories",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(80)",
+                oldMaxLength: 80)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "ImagemUrl",
+                table: "categories",
+                type: "longtext",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "varchar(300)",
+                oldMaxLength: 300)
+                .Annotation("MySql:CharSet", "utf8mb4")
+                .OldAnnotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_products",
+                table: "products",
+                column: "ProductId");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_categories",
+                table: "categories",
+                column: "CategoryId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_products_categories_CategoryId",
+                table: "products",
+                column: "CategoryId",
+                principalTable: "categories",
+                principalColumn: "CategoryId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
