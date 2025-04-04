75ca08fa3bc7ebbb496d5073a356b6d81f92a416
Arthur Teixeira <arthurteixeira.guts@gmail.com>
2025-04-03 21:07:26 -0300

    Add ProductController for managing product requests
    Introduces a new `ProductController` class in the `web_api_catalog.Controllers` namespace. This controller handles HTTP GET requests to the "api/products" endpoint, retrieves products from the database using `AppDbContext`, and returns appropriate responses based on the availability of products.

---
+++ web-api-catalog/web-api-catalog/Controllers/ProductController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using web_api_catalog.Context;
+
+namespace web_api_catalog.Controllers;
+
+public class ProductController : ControllerBase
+{
+    private readonly AppDbContext _context;
+
+    public ProductController(AppDbContext context)
+    {
+        _context = context; 
+    }
+
+    [HttpGet]
+    [Route("api/[controller]")]
+    public IActionResult Get()
+    {
+
+        var products = _context.products.ToList();
+        if (products == null || !products.Any())
+        {
+            return NotFound("No products found.");
+        }
+        return Ok(products);
+    }
+}
